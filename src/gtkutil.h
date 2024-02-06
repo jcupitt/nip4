0a4f517b03ebdbae43a45ad40384f6299ce1fe33
John Cupitt <jcupitt@gmail.com>
2024-02-06 09:31:01 +0000

    add GPL boilerplate
    and copy view.[hc] in

--- src/gtkutil.h
+++ src/gtkutil.h
@@ -1,3 +1,24 @@
+/*
+
+    Copyright (C) 1991-2003 The National Gallery
+    Copyright (C) 2004-2023 libvips.org
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+ */
+
 #define WHITESPACE " \t\r\b\n"
 
 void set_gentryv(GtkWidget *edit, const char *fmt, va_list ap);
@@ -17,7 +38,7 @@ void block_scroll(GtkWidget *widget);
 
 gboolean widget_should_animate(GtkWidget *widget);
 
-void action_toggle(GSimpleAction *action, 
+void action_toggle(GSimpleAction *action,
 	GVariant *parameter, gpointer user_data);
-void action_radio(GSimpleAction *action, 
+void action_radio(GSimpleAction *action,
 	GVariant *parameter, gpointer user_data);
