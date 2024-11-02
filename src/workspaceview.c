8b1434c54c640c14b81010bc220637a59bddd37e
John Cupitt <jcupitt@gmail.com>
2024-11-02 13:05:06 +0000

    restyle source column on drag start

--- src/workspaceview.c
+++ src/workspaceview.c
@@ -893,13 +893,14 @@ workspaceview_float_rowview(Workspaceview *wview, Rowview *rview)
 
 	g_object_unref(rview);
 
-	// unstyle the floating row
+	// no corner styling on the row we drag
 	gtk_widget_remove_css_class(rview->frame, "top");
 	gtk_widget_remove_css_class(rview->frame, "bottom");
 
 	// force a refresh to get everything in the right place
 	vobject_refresh(VOBJECT(wview->drag_rview));
 	vobject_refresh(VOBJECT(wview->floating));
+	vobject_refresh(VOBJECT(wview->old_sview));
 }
 
 static void
